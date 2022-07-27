cae4e252cfd8673adefe03ba0d01a7626a3f376b
Abdoulaye BARRY <barryab12@gmail.com>
2022-07-27 09:15:26 +0000

    Readme added

---
+++ README.md
@@ -0,0 +1,21 @@
+# README
+
+### Spécifications
+
+- Avoir python 3 ou plus.
+- Avoir docker et docker-compose installés
+- Avoir pip
+
+### Etapes
+
+1. Installer les requierements
+
+```
+pip3 install -r requirements.txt
+```
+
+2. Executer le code python
+
+```
+python3 index.py
+```
--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import subprocess, gdown
+import subprocess, gdown, os.path
 from getpass import getpass
+import platform
 
 print('Vos informations GITLAB de connexion vous seront demandées.')
 username = input('User: ')
@@ -12,19 +13,24 @@ url_docker_compose = "git clone https://github.com/barryab12/openmoise-docker.gi
 filestore_link = "https://drive.google.com/uc?id=1jMHuiniqvX11xJAlUzZtHvZM0eJPHnFk"
 
 print("-- Téléchargement des images et fichiers documents ... ")
-gdown.download(filestore_link, 'filestore.zip', quiet=False)
+if not os.path.isdir('filestore/'):
+    gdown.download(filestore_link, 'filestore.zip', quiet=False)
 
-print("-- Téléchargement des modules ")
-subprocess.run(url_addons, shell=True)
+print("-- Téléchargement des modules")
+if not os.path.isdir('addons/'):
+    subprocess.run(url_addons, shell=True)
 
 print("-- Téléchargement des fichiers de configurations ")
 subprocess.run(url_docker_compose, shell=True)
 subprocess.run('mv openmoise-docker/* .', shell=True)
 subprocess.run('rm -fr openmoise-docker/', shell=True)
-subprocess.run('unzip filestore.zip', shell=True)
-
+if not os.path.isfile('filestore.zip'):
+    subprocess.run('unzip filestore.zip', shell=True)
 print("-- Suppression des fichiers ... ")
 subprocess.run('rm -fr filestore.zip', shell=True)
-
 print("-- Lancement des conteneurs")
-subprocess.run('docker-compose up -d', shell=True)
+if os.path.isfile('docker-compose.yml') or os.path.isfile(
+        'docker-compose.yaml'):
+    subprocess.run('docker-compose up -d', shell=True)
+else:
+    print('le fichier docker-compose est inexistant')
